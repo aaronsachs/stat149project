ed14883bab3c036863fd4fae4d1c54624e47d3cf
Nuralk <lurbisaia@gmail.com>
2019-05-05 17:31:13 -0400

    added poisson hurdle

--- Poisson.R
+++ Poisson.R
@@ -1,6 +1,8 @@
 if (!require(aod)) {install.packages("aod"); require(aod)}
 library(MASS)
 library(stats)
+library(car)
+library(pscl)
 
 source('~/Documents/STAT149/proyecto/lucas/Functions.R')
 
@@ -18,42 +20,54 @@ ami <- ami[ami$CHARGES > 300,]
 
 # DRG:AGE, DRG:LOGCHARGES, DIAGNOSIS:LOGCHARGES, DIAGNOSES:SEX, 
 
+
 #### with dropped data ########
 model_str1 <- 'glm('
-model_str2 <- ',family = poisson, data = ami)'
-predictors <- c('DIAGNOSIS' , 'SEX', 'DRG', 'log(CHARGES + 2000)' , 'AGE', 'LOGCHARGES.na', 'DRG:AGE', 'DRG:LOGCHARGES', 'DIAGNOSIS:LOGCHARGES', 'DIAGNOSIS:SEX')
+model_str2 <- ',family = \'poisson\', data = ami)'
+predictors <- c('DIAGNOSIS' , 'SEX', 'DRG', 'log(CHARGES + 2000)' , 'AGE', 'LOGCHARGES.na')
+                #, 'DRG:AGE', 'DRG:LOGCHARGES', 'DIAGNOSIS:LOGCHARGES', 'DIAGNOSIS:SEX', 'DIAGNOSIS:DRG')
 dependent.name <- "LOS"
 
 # find the best model
-poisson.bm.str <- best_model(model_str1, model_str2, dependent.name, predictors, data = ami, test = 'F')
-poisson.bm <- glm(LOS ~ log(CHARGES + 2000) + DRG:AGE +  DIAGNOSIS:LOGCHARGES + DRG + DRG:LOGCHARGES + SEX + DIAGNOSIS + LOGCHARGES.na + DIAGNOSIS:SEX , family = 'poisson', data = ami)
+poisson.bm.str <- best_model(model_str1, model_str2, dependent.name, predictors, data = ami, test = 'Chisq')
+poisson.bm <- glm(LOS ~ 1 + log(CHARGES + 2000) + DRG + AGE + DIAGNOSIS + SEX + 
+                  LOGCHARGES.na, family = 'poisson', data = ami)
 
 cooks <- cooks.distance(poisson.bm)
+summary(ami
+        )
 
+vif(poisson.bm)
 
+#######################################################################################
+##############################          POISSON       #################################
+##############################           HURDLE        ################################
+#######################################################################################
 
-load.data <- function(path){
-  print(paste(path, 'amidata.csv', sep = ''))
-  ami <- read.csv(paste(path, 'amidata.csv', sep =''))
-  
-  # drop this value
-  ami <- ami[ami$LOS != 0, ]
-  
-  ami$LOGCHARGES <- log(ami$CHARGES)
-  
-  # drop died
-  ami$DIED <- NULL
-  
-  # convert
-  ami <- na.convert.mean(ami)
-  
-  # turn specific columns to factors
-  factor.columns <- c("Patient", "DIAGNOSIS", 'SEX', 'DRG', 'LOGCHARGES.na')
-  ami[, factor.columns] <- data.frame(apply(ami[factor.columns], 2, as.factor))
-  
-  return(ami)
-  
+model_str1 <- 'hurdle('
+model_str2 <- ',dist = \'poisson\', data = ami)'
+predictors <- c('DIAGNOSIS' , 'SEX', 'DRG', 'log(CHARGES + 2000)' , 'AGE', 'LOGCHARGES.na', 'DRG:AGE', 'DRG:LOGCHARGES', 'DIAGNOSIS:LOGCHARGES', 'DIAGNOSIS:SEX', 'DIAGNOSIS:DRG')
+dependent.name <- "LOS-1"
+
+# find the best model
+poisson.bm.str <- best_model(model_str1, model_str2, dependent.name, predictors, data = ami, test = 'Chisq')
+poisson.bm <- glm(LOS-1 ~ 1 + log(CHARGES + 2000) + DRG + AGE + DIAGNOSIS + SEX + 
+                    LOGCHARGES.na, family = 'poisson', data = ami)
+
+mod1 = hurdle(LOS-1~DIAGNOSIS,dist='poisson',data=ami)
+mod2 = hurdle(LOS-1~DIAGNOSIS+AGE,dist='poisson',data=ami)
+logLik(mod1)
+logLik(mod2)
+
+a5 <- apply(cbind(paste("(",names(ami[6]),"-1)~"),data.frame(t(combn(predictors, 10)))), 1, paste, collapse="+")
+models5 <- lapply(a5,FUN = function(X) hurdle(X, dist="poisson",data=ami))
+
+results <- data.frame(model = c(), deviance = c(), coefficients = c(), least_deviance = c())
+buffer <- data.frame(model = c(), deviance = c(), coefficients = c(), least_deviance = c())
+for(i in 1:length(models5)){
+  newdf <- data.frame(model = models5[[i]][['formula']],deviance = models5[[i]][['deviance']], coefficients = length(models5[[i]][["coefficients"]]), least_deviance = 0)
+  buffer <- rbind(buffer,newdf)
 }
-
-
-
+buffer$least_deviance[which.min(buffer$deviance)] <- 1
+results <- rbind(results,buffer)
+buffer <- data.frame(model = c(), deviance = c(), coefficients = c(), least_deviance = c())
